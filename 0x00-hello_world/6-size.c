65fea291932663e0408262db68679abf64bcd71e
hiba-M <hiba.mouzain@gmail.com>
2023-02-16 14:57:21 -0800

    fix bug

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 /**
- *
- *
- * /
- int main(void)
- {
- 	printf();
-	return (0);
-
- }
+  * main - this prints sizeof types
+  * Return: This returns 0
+  */
+int main(void)
+{
+char a;
+int q;
+long int g;
+long long int n;
+float o;
+printf("Size of a char: %lu byte(s)\n", sizeof(a));
+printf("Size of an int: %lu byte(s)\n", sizeof(q));
+printf("Size of a long int: %lu byte(s)\n", sizeof(g));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(n));
+printf("Size of a float: %lu byte(s)\n", sizeof(o));
+return (0);
+}
