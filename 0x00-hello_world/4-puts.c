119b8753c3d9db79d54c1c9c80393cdb1e75e4de
hiba-M <hiba.mouzain@gmail.com>
2023-02-16 13:40:57 -0800

    fix bug

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(){
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+/*
+ *print string 
+ *return : 0 == succsess
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
