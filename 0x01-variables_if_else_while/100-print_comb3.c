4fc1ea4187101630eecb495bb826d9ec55e9ce52
hiba-M <hiba.mouzain@gmail.com>
2023-02-17 14:36:10 -0800

    fix#10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,13 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 48; i < 58; i++)
+	for (i = 48; i < 57; i++)
 	{
 		for (j = 48; j < 58; j++)
 		{
 			putchar(i);
 			putchar(j);
-			if (i != 57 || j != 57)
+			if (i != 56 || j != 57)
 			{
 				putchar(',');
 				putchar(' ');
