39b94b97bfe80503fe5d2102d2123f07f40f6479
hiba-M <hiba.mouzain@gmail.com>
2023-02-17 11:47:58 -0800

    fix #1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/** 
- * main - find the last number whether greater than five
- * less than six or equal to zero
- * Return: Always 0
-*/
+/**
+ * main - Entry point
+ *
+ * Description: 'wheter it is grater than five
+ *
+ * or less than six .'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
