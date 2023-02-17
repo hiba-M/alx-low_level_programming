2ce436b0b6e5217a821bb9bd630a1fbdfeb8b39a
hiba-M <hiba.mouzain@gmail.com>
2023-02-17 11:54:42 -0800

    fix #2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'from a to z lower case'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (char i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
