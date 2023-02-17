6d7fc489431ba2cc65a6cc643b162ff01dab09b1
hiba-M <hiba.mouzain@gmail.com>
2023-02-17 14:06:54 -0800

    fix#9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'single_digit seperate by comma'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
