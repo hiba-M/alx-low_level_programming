2ee8bf2d48fc4cebf3ca41f7e436d518be34eaa6
hiba-M <hiba.mouzain@gmail.com>
2023-02-19 04:13:07 -0800

    2digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,16 +9,19 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 48; i < 57; i++)
+	for ( i = 48; i < 57; i++)
 	{
-		for (j = 49; j < 58; j++)
+		for ( j = 49; j < 58; j++)
 		{
-			putchar(i);
-			putchar(j);
-			if (i != 56 || j != 57)
+			if (i < j)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(i);
+				putchar(j);
+				if (i != 56 || j != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
