1078a3c0fd9c2c44f09180c05059b17857a87c31
hiba-M <hiba.mouzain@gmail.com>
2023-02-21 07:51:38 -0800

    alphab

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alphabets [a-z]
+ * Return: 1
+ */
+int main(void)
+void print_alphabet(void)
+{
+	int i;
+
+	for (int i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
