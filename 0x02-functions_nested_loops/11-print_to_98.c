0e1c7ad855dec7d539e508abadc6f6d29795cc8e
hiba-M <hiba.mouzain@gmail.com>
2023-02-21 13:27:52 -0800

    to98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - print number to 98
+ * @n: number given
+ * Return: non
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < '98')
+	{
+		for (i = n; i < '98' ; i++)
+		{
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		for (i = n; i > '98'; i--)
+		{
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
