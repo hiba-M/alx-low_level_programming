796d20af8b4acabb10cfefd6660b897cfe24e650
hiba-M <hiba.mouzain@gmail.com>
2023-02-21 13:30:27 -0800

    to98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,9 +9,9 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n < '98')
+	if (n < 98)
 	{
-		for (i = n; i < '98' ; i++)
+		for (i = n; i < 98 ; i++)
 		{
 			_putchar(i + '0');
 			_putchar(',');
@@ -20,7 +20,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (i = n; i > '98'; i--)
+		for (i = n; i > 98; i--)
 		{
 			_putchar(i + '0');
 			_putchar(',');
