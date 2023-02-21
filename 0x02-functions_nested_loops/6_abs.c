2e9443b4b4db6b724b887a598be6d87e7701e39a
hiba-M <hiba.mouzain@gmail.com>
2023-02-21 12:00:40 -0800

    abs

---
+++ 0x02-functions_nested_loops/6_abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - the absolute value of n
+ * @n: number given
+ * Return: 0 (success)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		_putchar(n);
+	}
+	else
+	{
+		_putchar(-n);
+	}
+}
