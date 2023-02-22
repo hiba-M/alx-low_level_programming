181258a516e6f6c6d2e1cc04a577168f8fbc56bf
hiba-M <hiba.mouzain@gmail.com>
2023-02-22 08:01:45 -0800

    table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * times_table - table of numbers from 0 to 9
+ * Return: none
+ */
+void times_table(void)
+{
+	int x, i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			x = i * j;
+			_putchar(x + '0');
+			_putchar(',');
+			_putchar('\n');
+		}
+	}
+}
