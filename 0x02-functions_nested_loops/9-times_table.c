e6470d3598b6af4ccb354f6f28155ef8dc8439ca
hiba-M <hiba.mouzain@gmail.com>
2023-02-22 08:21:30 -0800

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,11 +12,17 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			x = i * j;
-			_putchar((x % 10) + '0');
-			if (i > 0 || i < 9)
+			if (x <= 9)
 			{
 				_putchar(' ');
 				_putchar(',');
+				_putchar(x + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(',');
+				_putchar((x % 10) + '0');
 			}
 		}
 		_putchar('\n');
