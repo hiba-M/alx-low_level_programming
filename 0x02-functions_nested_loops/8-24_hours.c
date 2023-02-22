85f1a6a83b2f4503d45860f5bd805e3fbfbe1fde
hiba-M <hiba.mouzain@gmail.com>
2023-02-22 05:13:10 -0800

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * jack_bauer - print the time from 00:00 to 23:59
+ * Return: non
+*/
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 23 ; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			if (i < 10 && j < 10)
+			{
+				_putchar('0', i + '0');
+				_putchar(':');
+				_putchar('0', j + '0');
+			}
+			else if (i < 10)
+			{
+				_putchar('0', i + '0');
+				_putchar(':');
+				_putchar(j + '0');
+			}
+			else if (j < 10)
+			{
+				_putchar(i + '0');
+				_putchar(':');
+				_putchar('0', j + '0');
+			}
+			else
+			{
+				_putchar(i + '0');
+				_putchar(':');
+				_putchar(j + '0');
+			}
+		}
+	}
+}
