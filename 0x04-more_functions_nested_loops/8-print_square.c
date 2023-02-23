ade2fada64984f5f111bc9fa016887f05fa0a0cf
hiba-M <hiba.mouzain@gmail.com>
2023-02-23 14:52:25 -0800

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - form a square of #
+ * @size: input given
+ * Return: none
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
