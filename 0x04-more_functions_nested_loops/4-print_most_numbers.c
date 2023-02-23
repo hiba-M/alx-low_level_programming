ae91af6a8145f87d76cb2a90119456f3c4b6ee1a
hiba-M <hiba.mouzain@gmail.com>
2023-02-23 09:38:42 -0800

    no 2 or 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - print from 0 to 9
+ * but 2 and 4
+ * Return: none
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (int i = 48; i < 58; i++)
+	{
+		if (i != 50 || i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
