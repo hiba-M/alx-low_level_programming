b75937d934ad5eb22da47ba706e7d08e089b0991
hiba-M <hiba.mouzain@gmail.com>
2023-02-23 08:41:35 -0800

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplication of 2 num
+ * @a: first number
+ * @b: second number
+ * Return: multiplication of the 2
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
