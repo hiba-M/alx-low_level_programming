d5bb773f80291cd71aff64cc9dd735c1ff94024c
hiba-M <hiba.mouzain@gmail.com>
2023-03-02 07:51:25 -0800

    concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - concat 2 string
+ * @dest: given input.
+ * @src: given input.
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j, count1;
+
+	for (i = 0; dest[i] != '\0' ; ++i)
+	{}
+	for (j = 0; dest[i] != '\0' ; ++j, ++i)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,14 @@
+# continuation of pointers, arrays and string .
+
+**Description**
+
+the files are c programing functions :
+
+- stract => concat 2 strings.
+- strncat
+- strncpy
+- strcmp
+- reverse arry 
+- to upper case
+- capitalizes string 
+- .....
