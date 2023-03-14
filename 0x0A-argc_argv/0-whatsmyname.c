9d506159762c0ae99c98b3e3f091fbb5f6cbc30e
hiba-M <hiba.mouzain@gmail.com>
2023-03-14 08:28:33 -0700

    tasks0,1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print the name of progress
+ * @argv: number of argument
+ * @argc: arrays of argument
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,6 @@
+# command line arguments.
+
+* There are atleast 2 argument *main:argc*,*main:argv*.
+The 1st: is the number of argument (the count).
+the 2sd: is an array of str golding each arg (each arg is passed as str).
+
