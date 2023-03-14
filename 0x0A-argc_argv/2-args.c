66501d06fd0465c4bcf113c8add6de0ad1aca778
hiba-M <hiba.mouzain@gmail.com>
2023-03-14 09:53:09 -0700

    localy tested

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print the name of progress
+ * @argv: number of argument
+ * @argc: arrays of argument
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
