dd07dc0d5dd66e40c749e26c60951195c5eb2c0e
hiba-M <hiba.mouzain@gmail.com>
2023-03-21 10:31:48 -0700

    betty and newT

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog represantation data structure
+ * @name: doggy name
+ * @age: doggy age
+ * @owner: doggy owner
+ */
+
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct god_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /*DOG_H*/
