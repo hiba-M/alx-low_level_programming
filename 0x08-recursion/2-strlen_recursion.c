828550fbbd193405fa6b3c1a42a5f55df72dad54
hiba <hiba.mouzain@gmail.com>
2023-06-07 23:33:32 +0100

    betty fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _strlen_recursion - the length of the string
  * @s: input given
- * Return :  lenth of s
+ * Return:  lenth of s
  */
 
 int _strlen_recursion(char *s)
@@ -15,7 +15,7 @@ int _strlen_recursion(char *s)
 	}
 	else
 	{
-		n = 1 + _strlen_recursion(s + 1); 
+		n = 1 + _strlen_recursion(s + 1);
 	}
-	return n;
+	return (n);
 }
