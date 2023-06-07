99fde9beb0fb358c539a2f169b6b411dd42e80aa
hiba <hiba.mouzain@gmail.com>
2023-06-07 23:10:58 +0100

    task 2

---
+++ 0x08-recursion/2-strlen_recusion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen_recursion - the length of the string
+ * @s: input given
+ * Return :  lenth of s
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		n = 1 + _strlen_recursion(s + 1); 
+	}
+	return n;
+}
