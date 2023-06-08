a329b96b7a190b3702415cdf1c0586dfaedc75ea
hiba <hiba.mouzain@gmail.com>
2023-06-08 22:59:12 +0100

    compiler

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,6 +7,8 @@
  */
 int _pow_recursion(int x, int y)
 {
+	int res;
+
 	if (y < 0)
 	{
 		return (-1);
@@ -21,6 +23,7 @@ int _pow_recursion(int x, int y)
 	}
 	else
 	{
-		x = x + _pow_recursion(x , y - 1);
+		res = x * _pow_recursion(x , y - 1);
+		return (res);
 	}
 }
