64a96839a7c86898b9278166a1ba05dc2df95e12
hiba <hiba.mouzain@gmail.com>
2023-06-09 00:51:03 +0100

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,21 +2,23 @@
 /**
  * _sqrt_recursion - function that give the square of a num
  * @n: int given 
- * Return: -1 in case of num  is odd else gives the square root.
+ * Return: -1 in case of num isn't natural else gives the square root.
  */
 int _sqrt_recursion(int n)
 {
-	int res;
-
-	if (n % 2 != 0)
-	{
+	return (natural_sqrt(1, n));
+}
+/**
+ * natural_sqrt - look for natural square root
+ * @i: number to be compared
+ * @n: number to check
+ * Return: natural square root of num
+ */
+int natural_sqrt(int i, int n)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
 		return (-1);
-	}
-	else if (n == 0)
-	{
-		return (0);
-	}
-	else
-	{
-	}
+	return (natural_sqrt(i + 1, n));
 }
