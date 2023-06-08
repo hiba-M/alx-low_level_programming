f6bb1434898130bc0af1f8bb8a66bd327860c205
hiba <hiba.mouzain@gmail.com>
2023-06-09 00:55:36 +0100

    compiler

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,6 @@
 #include "main.h"
 /**
- * _sqrt_recursion - function that give the square of a num
- * @n: int given 
- * Return: -1 in case of num isn't natural else gives the square root.
- */
-int _sqrt_recursion(int n)
-{
-	return (natural_sqrt(1, n));
-}
-/**
- * natural_sqrt - look for natural square root
+ * natural_sqrt - look for natural square root 
  * @i: number to be compared
  * @n: number to check
  * Return: natural square root of num
@@ -21,4 +12,13 @@ int natural_sqrt(int i, int n)
 	if (i * i > n)
 		return (-1);
 	return (natural_sqrt(i + 1, n));
+}
+/**
+ * _sqrt_recursion - function that give the square of a num
+ * @n: int given 
+ * Return: -1 in case of num isn't natural else gives the square root.
+ */
+int _sqrt_recursion(int n)
+{
+	return (natural_sqrt(1, n));
 }
