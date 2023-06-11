6e065dcf6b5a4ef6b720bb94752ce868b5d0f99d
hiba <hiba.mouzain@gmail.com>
2023-06-11 23:06:06 +0100

    test

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main- program that adds positive numbers.
+ * @argc:number of command line
+ * @argv:array commande line
+ * Return: number
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+	if (argc == 1)
+	{
+		printf("%d\n",0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum+=atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+	return (0);
+}
