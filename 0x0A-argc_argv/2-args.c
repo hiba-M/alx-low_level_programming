a3306c7b9b2da4144a9dbbd343a8b4b206c27f9c
hiba <hiba.mouzain@gmail.com>
2023-06-11 21:52:16 +0100

    task3

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
  * main - print the name of progress
  * @argv: arrays of argument
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print the result of two numbers
+ * @argv: arrays of argument
+ * @argc: number of argument
+ * Return: 0 (success) 1 (less or more than 2arg)
+ */
+int main(int argc, char * argv[])
+{
+    int i, res = 1;
+
+    if ( argc == 3)
+    {
+	    for (i = 1; i < argc; ++i)
+	    {
+		    res = res * argv[i];
+	    }
+	    printf("%d\n",res);
+    }
+    else
+    {
+	    printf("Error\n");
+	    return (1);
+    }
+    return (0);
+}
