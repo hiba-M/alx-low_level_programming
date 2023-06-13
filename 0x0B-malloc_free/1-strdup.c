d5d7f278232f483aa9016d6e98594a421ab93416
hiba <hiba.mouzain@gmail.com>
2023-06-13 23:23:28 +0100

    Task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _strdup - print put the copy of str given
+ * @str: pointer of str
+ * Return: NULL if tehere is no str else duplicate str
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int cnt, i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		cnt = 0;
+		while ( * (str + cnt) != '\0')
+		{
+			cnt++
+		}
+		dup = malloc(cnt + 1);
+
+		if (dup == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i <= cnt; i++)
+			{
+				dup[i] = str [i];
+			}
+			return (dup);
+		}
+	}
+}
