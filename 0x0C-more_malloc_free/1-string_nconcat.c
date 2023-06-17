8748984e960deab4c46b55517df56caf1e3c86fe
hiba-M <hiba.mouzain@gmail.com>
2023-06-16 17:09:30 -0700

    t1 compiler

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,27 +11,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *p, *save;
 	unsigned int i, len1, len2;
 
-	if (s1 != NULL)
-		len1 = strlen(s1);
-	else
-		len1 = 0;
-	if (s2 != NULL)
-		len2 = strlen(s2);
-	else
-		len2 = 0;
+	len1 = (s1 != NULL) ? strlen(s1) : 0;
+	len2 = (s2 != NULL) ? strlen(s2) : 0;
 	p = malloc(len1 + 1 + n);
 	if (p == NULL)
 		return (NULL);
 	save = p;
 	if (s1 != NULL)
+	{
 		while ((*p = *s1))
+		{
 			s1++;
 			p++;
+		}
+	}
 	if (s2 != NULL)
+	{
 		for (i = 0; i < n && i <= len2; i++)
+		{
 			*p = *s2;
 			p++;
 			s2++;
+		}
+	}
 	*p = '\0';
 	return (save);
 }
