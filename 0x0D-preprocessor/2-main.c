7d1d4f4d8c959c37d586d88527c512d7d1ec64a3
hiba <hiba.mouzain@gmail.com>
2023-06-20 23:08:52 +0100

    preprossessing

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the name of the file that was compiled..
+ *
+ * Return: Always 0 means Success.
+ */
+int main()
+{
+    printf("Name of file: %s\n", __FILE__);
+
+    return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,6 @@
+# preprocessor in c
+Tasks related to processing.
+- creating files related to processing
+- creatingheader 
+- compaling
+
